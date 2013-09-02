5578f8ab23b670cafd7b9fff5c049e9c27889250
Rob Zyskowski <zyskowski.rob@gmail.com>
2013-09-02 08:56:34 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwentyFilms::Application.config.secret_token = '9f9bdb80b8bb4e6e9bc96cb31e9c78829faf1d33111cfa80e8e9f77552e964b1743d56cd52a259b6ba7a79d81859540b5b2a8d949305d8f3d9b1b358b314803f'
